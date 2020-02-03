1b8a8ec535319c71e80bc59eca045afc96af929e
Mazhar Ali <mazharali8251@gmail.com>
2020-02-03 21:32:44 +0500

    second Cmmit

--- app/src/main/java/com/example/githubpract/MainActivity.kt
+++ app/src/main/java/com/example/githubpract/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.githubpract
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this,"Mahzar Ali",Toast.LENGTH_SHORT).show()
     }
 }
